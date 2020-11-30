c4a9c15f607a3f074d6a82572e93ee4d68f62a5f
gabriel <gabriellopes112009@gmail.com>
2020-11-30 14:34:06 -0300

    produc overview screen grid implementation

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product_model.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
+  final List<ProductModel> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('minha loja'),
+        ),
+        body: GridView.builder(
+          itemCount: loadedProducts.length,
+          padding: const EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3/2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10
+          ),
+          itemBuilder: (context, index) => Text(loadedProducts[index].title),
+        ));
   }
 }
