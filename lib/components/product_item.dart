b6b406ed6dc5f098cafbf8dad3551c722395cd37
gabriel <gabriellopes112009@gmail.com>
2020-12-12 21:25:13 -0300

    Font definition, roundend GridTile

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -8,24 +8,32 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
+    Color accentColor = Theme.of(context).accentColor;
+    return ClipRRect( //Make component Rounded
+    borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: (){},
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: accentColor,
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: accentColor,
+            ),
+            onPressed: () {},
+          ),
         ),
       ),
     );
