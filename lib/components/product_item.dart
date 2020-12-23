04d504440488f33b0ae25168ac5b08e750c4232e
gabriel <gabriellopes112009@gmail.com>
2020-12-23 17:46:00 -0300

    listen propertie of Provider.of

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -5,20 +5,20 @@ import 'package:shop/screens/produtc_detail._screen.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final ProductModel product = Provider.of<ProductModel>(context);
+    final ProductModel product = Provider.of<ProductModel>(
+      context,
+      listen: true,
+    );
     Color accentColor = Theme.of(context).accentColor;
     return ClipRRect(
       //Make component Rounded
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
         onTap: () {
-          Navigator.of(context).pushNamed(
-            AppRoutes.PRODUCT_DETAIL,
-            arguments: product
-          );
+          Navigator.of(context)
+              .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
         },
         child: GridTile(
           child: Image.network(
@@ -29,7 +29,8 @@ class ProductItem extends StatelessWidget {
             backgroundColor: Colors.black87,
             leading: IconButton(
               color: accentColor,
-              icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
               onPressed: () {
                 product.toogleFavorite();
               },
