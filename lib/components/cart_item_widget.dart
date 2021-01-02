976c29823837fa0f9a7882e8ba44401c2b42b49c
gabriel <gabriellopes112009@gmail.com>
2021-01-02 18:44:41 -0300

    dismissible implementation

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/providers/cart_provider.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,36 +9,56 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) => Provider.of<Cart>(context, listen: false)
+          .removeItem(cartItem.productId),
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: Container(
-            height: MediaQuery.of(context).size.height * 0.05,
-            width: MediaQuery.of(context).size.width * 0.2,
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(2),
-              child: FittedBox(
-                child: Text(
-                  'R\$ ${cartItem.price}',
-                  style: TextStyle(
-                    color: Colors.white,
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: Container(
+              height: MediaQuery.of(context).size.height * 0.05,
+              width: MediaQuery.of(context).size.width * 0.2,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                child: FittedBox(
+                  child: Text(
+                    'R\$ ${cartItem.price}',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
+            title: Text(cartItem.title),
+            subtitle: Text('Total: R\$ ${cartItem.quantity * cartItem.price}'),
+            trailing: Text('${cartItem.quantity} X'),
           ),
-          title: Text(cartItem.title),
-          subtitle: Text('Total: R\$ ${cartItem.quantity * cartItem.price}'),
-          trailing: Text('${cartItem.quantity} X'),
         ),
       ),
     );
